0e66fc12e4321b616c96e79d850f6eddae0fc563
stumbaumr <stumbaumr@yahoo.de>
2025-01-09 11:59:17 +0100

    Fix typo in commands_test.go

--- internal/tesla/commands/commands_test.go
+++ internal/tesla/commands/commands_test.go
@@ -7,12 +7,12 @@ import (
 	"github.com/teslamotors/vehicle-command/pkg/vehicle"
 )
 
-func TestAllExceptedCommandsImplemented(t *testing.T) {
+func TestAllExpectedCommandsImplemented(t *testing.T) {
 	// Get all case statements from the Send method by creating a dummy command
 	// and checking which commands return "unrecognized command"
 	command := &Command{}
 
-	for _, expectedCmd := range ExceptedCommands {
+	for _, expectedCmd := range ExpectedCommands {
 		command.Command = expectedCmd
 		car := &vehicle.Vehicle{}
 		ctx := context.Background()
@@ -22,7 +22,7 @@ func TestAllExceptedCommandsImplemented(t *testing.T) {
 		_, err := command.Send(ctx, car)
 
 		if err != nil && err.Error() == "unrecognized command: "+expectedCmd {
-			t.Errorf("Command %q is in ExceptedCommands but not implemented in Send method", expectedCmd)
+			t.Errorf("Command %q is in ExpectedCommands but not implemented in Send method", expectedCmd)
 		}
 	}
 }
